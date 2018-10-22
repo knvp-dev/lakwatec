5c371dd313f4c0ed56f77677f24f0ac414670bc2
Kenny Vanpaemel <Kenny.Vanpaemel@axsit.be>
2018-10-22 10:05:57 +0200

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,52 +8,52 @@
     <div class="container item-columns">
         <div class="columns is-multiline is-mobile is-centered is-gapless">
             <div class="column">
+            <a href="{{ route('pompinstallaties') }}">
                 <div class="icon-item">
-                    <a href="{{ route('pompinstallaties') }}">
                         <img src="/images/icons/pump.png" alt="">
                         <span class="icon-item-text">Pompinstallaties</span>
-                    </a>
                 </div>
+                </a>
             </div>
             <div class="column">
+            <a href="{{ route('boorputten') }}">
                 <div class="icon-item">
-                    <a href="{{ route('boorputten') }}">
                         <img src="/images/icons/drill.png" alt="">
                         <span class="icon-item-text">Boorputten</span>
-                    </a>
                 </div>
+                </a>
             </div>
             <div class="column">
+            <a href="{{ route('beregeningen') }}">
                 <div class="icon-item">
-                    <a href="{{ route('beregeningen') }}">
                         <img src="/images/icons/sprinkler.png" alt="">
                         <span class="icon-item-text">Beregeningsinstallaties</span>
-                    </a>
                 </div>
+                </a>
             </div>
             <div class="column">
+            <a href="{{ route('beregeningen') }}">
                 <div class="icon-item">
-                    <a href="{{ route('beregeningen') }}">
                         <img src="/images/icons/garden-hose.png" alt="">
                         <span class="icon-item-text">Beregeningsinstallaties landbouw</span>
-                    </a>
                 </div>
+                </a>
             </div>
             <div class="column">
+            <a href="{{ route('water-en-gierslangen') }}">
                 <div class="icon-item">
-                    <a href="{{ route('water-en-gierslangen') }}">
                         <img src="/images/icons/hose.png" alt="">
                         <span class="icon-item-text">Water & gierslangen</span>
-                    </a>
                 </div>
+                </a>
                 </div>
             <div class="column">
+            <a href="{{ route('pe-lassen')">
                 <div class="icon-item">
-                <a href="{{ route('pe-lassen')">
                     <img src="/images/icons/welding.png" alt="">
                     <span class="icon-item-text">PE Lassen</span>
-                    </a>
                 </div>
+                </a>
             </div>
         </div>
     </div>
